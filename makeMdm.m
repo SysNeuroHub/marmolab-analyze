145c4fd31546f6892d7647202b71ab75965ffad1
Maureen Hagan <maureen.hagan@monash.edu>
2023-02-10 13:05:21 +1100

    updated makeMdm to work with new marmodata branch - now works with multiple files and can load spikes and lfps simultaneously!

--- makeMdm.m
+++ makeMdm.m
@@ -18,15 +18,18 @@ function d = makeMdm(varargin)
 p = inputParser();
 p.KeepUnmatched = true;
 p.addParameter('path','',@(x) ischar(x) || isempty(x)); % location of your data
+p.addParameter('savepath','',@(x) ischar(x) || isempty(x)); % location of your mdm files
 p.addParameter('subject','', @(x) ischar(x) || isempty(x));  % subject name
 p.addParameter('paradigm','',@(x) ischar(x) || isempty(x)); % paradigm name 
 p.addParameter('channels',[]);
 p.addParameter('analysis','',@(x) ischar(x) || isempty(x));
-p.addParameter('loadEye',true,@(x) validateattributes(x,{'logical'},{'scalar'}));
-p.addParameter('spikes',true,@(x) validateattributes(x,{'logical'},{'scalar'}));
-p.addParameter('source','ghetto',@(x) ischar(x) || isempty(x));
-p.addParameter('lfp',true,@(x) validateattributes(x,{'logical'},{'scalar'}));
+p.addParameter('eye',false,@(x) validateattributes(x,{'logical'},{'scalar'}));
+p.addParameter('spikes','', @(x)  ischar(x) || isempty(x));
+p.addParameter('lfp','',@(x) ischar(x) || isempty(x));
+p.addParameter('filtertype','multitaper',@(x) ischar(x) || isempty(x));
+p.addParameter('cfg','',@(x) ischar(x) || isempty(x)); % 'marmodata.cfgs.mbi.A1x32_100um' for centre array 'marmodata.cfgs.mbi.A1x32_Edge_100um'
 p.addParameter('reload',true,@(x) validateattributes(x,{'logical'},{'scalar'})); % eventually need to sort this out
+p.addParameter('overwrite',true,@(x) validateattributes(x,{'logical'},{'scalar'})); % eventually need to sort this out
 
 p.parse(varargin{:});
 
@@ -47,69 +50,112 @@ for ifile = 1:numel(Files)
 end
 
 % file name to save out to:
-if strcmp(args.source,'kilo')
-    savefilename = [args.path filesep args.subject '.' args.paradigm  '.kilo.mdm']; % save name > for sorted data
-else
-    savefilename = [args.path filesep args.subject '.' args.paradigm  '.mdm']; % save name
+if isempty(args.savepath)
+    args.savepath = args.path;
 end
 
-if exist(savefilename,'file')% if it already exits, load it so you dont have to do everything again
-    load(savefilename, '-mat', 'd'); 
+if strcmp(args.spikes,'kilo')
+    savefilename = [args.savepath filesep args.subject '.' args.paradigm  '.kilo.mdm']; % save name > for sorted data
 else
+    savefilename = [args.savepath filesep args.subject '.' args.paradigm  '.mdm']; % save name
+end
+
+% old method doesnt work for mulitple files - new branch should allow us to
+% do it at once... let see how it goes...
+
+% ns file only
+if ~args.eye && isempty(args.spikes) && isempty(args.lfp)
     d = marmodata.mdbase(filenames,'path', args.path);
-end 
-
-% step 2 - load the behaviour
-
-if args.loadEye
-    d = d.load('loadEye',args.loadEye);
-    
-    save(savefilename,'d')
-    disp('behaviour saved!')
+    fprintf('returning ns file only!')
 end
 
-% step 3 - if kilo, load spike data
-
-if args.spikes && strcmp(args.source,'kilo')
-    d = d.load('spikes',args.spikes,'source','kilo');
-    save(savefilename,'d')
-    disp('kilo spike data saved!')
+% eye data only
+if args.eye && isempty(args.spikes) && isempty(args.lfp)
+    d = marmodata.mdbase(filenames,'path', args.path,'loadArgs',{'eye',args.eye});
+    fprintf('returning eye and ns data only!')
 end
 
-% step 4 load lfps (and possibly spikes ghetto) channel by channel
+% spiking only
+if args.eye && ~isempty(args.spikes) && isempty(args.lfp)
+    assert(exist(args.cfg,'class') == 8,'''cfg'' must be the name of a valid ephys configuration, e.g., from marmodata.cfgs.');
+     
+    d = marmodata.mdbase(filenames,'path', args.path,'loadArgs',{'eye',args.eye,'spikes',args.spikes,'channels',args.channels,'cfg',args.cfg,'reload', args.reload});
+    fprintf(['returning ' args.spikes ' spikes'])
+end
 
-if ~isempty(args.channels)
-    channels = args.channels;
+% lfp only
+if args.eye && isempty(args.spikes) && ~isempty(args.lfp)
+    assert(exist(args.cfg,'class') == 8,'''cfg'' must be the name of a valid ephys configuration, e.g., from marmodata.cfgs.');
+     
+    d = marmodata.mdbase(filenames,'path', args.path,'loadArgs',{'eye',args.eye,'lfp',args.lfp,'filtertype',args.filtertype,'channels',args.channels,'cfg',args.cfg,'reload', args.reload});
+    fprintf('returning lfp data')
+end
 
-    if args.spikes && strcmp(args.source,'ghetto')
-        d = d.load('spikes',args.spikes,'source','ghetto','channels',channels,'reload', args.reload);
-        save(savefilename,'d')
-        disp('ghetto spike data saved!')
-    end
-    
-    if args.lfp
-        d = d.load('lfp',args.lfp,'channels',channels,'reload', args.reload);
-        save(savefilename,'d')
-        disp('lfp data saved!')
-    end
-    
-    % nice idea but this doesnt work
-%     for ich = 1:numel(channels)
-%         channel = channels(ich);
-%         
-%         if args.spikes && strcmp(args.source,'ghetto')
-%             d = d.load('spikes',args.spikes,'source','ghetto','channels',channel,'reload', args.reload);
-%             save(savefilename,'d')
-%             disp(['ghetto spike data for ch ' num2str(channel) ' saved!'])
-%         end
-%         
-%         if args.lfp 
-%             d = d.load('lfp',args.lfp,'channels',channel,'reload', args.reload);
-%             save(savefilename,'d')
-%             disp(['lfp data for ch ' num2str(channel) ' saved!'])
-%         end
+% everything (preferable!)
+if args.eye && ~isempty(args.spikes) && ~isempty(args.lfp)
+    assert(exist(args.cfg,'class') == 8,'''cfg'' must be the name of a valid ephys configuration, e.g., from marmodata.cfgs.');
+     
+    d = marmodata.mdbase(filenames,'path', args.path,'loadArgs',{'eye',args.eye,'spikes',args.spikes,'lfp',args.lfp,'filtertype',args.filtertype,'channels',args.channels,'cfg',args.cfg,'reload', args.reload});
+    fprintf(['returning ' args.spikes ' spikes and lfp data'])
+end
+
+% if exist(savefilename,'file')% if it already exits, load it so you dont have to do everything again
+%     load(savefilename, '-mat', 'd'); 
+% else
+%     d = marmodata.mdbase(filenames,'path', args.path);
+% end 
+% 
+% % step 2 - load the behaviour
+% 
+% if args.loadEye
+%     d = d.load('loadEye',args.loadEye);
+%     
+%     save(savefilename,'d')
+%     disp('behaviour saved!')
+% end
+% 
+% % step 3 - if kilo, load spike data
+% 
+% if args.spikes && strcmp(args.source,'kilo')
+%     d = d.load('spikes',args.spikes,'source','kilo');
+%     save(savefilename,'d')
+%     disp('kilo spike data saved!')
+% end
+% 
+% % step 4 load lfps (and possibly spikes ghetto) channel by channel
+% 
+% if ~isempty(args.channels)
+%     channels = args.channels;
+% 
+%     if args.spikes && strcmp(args.source,'ghetto')
+%         d = d.load('spikes',args.spikes,'source','ghetto','channels',channels,'reload', args.reload);
+%         save(savefilename,'d')
+%         disp('ghetto spike data saved!')
 %     end
-end
+%     
+%     if args.lfp
+%         d = d.load('lfp',args.lfp,'channels',channels,'reload', args.reload);
+%         save(savefilename,'d')
+%         disp('lfp data saved!')
+%     end
+%     
+%     % nice idea but this doesnt work
+% %     for ich = 1:numel(channels)
+% %         channel = channels(ich);
+% %         
+% %         if args.spikes && strcmp(args.source,'ghetto')
+% %             d = d.load('spikes',args.spikes,'source','ghetto','channels',channel,'reload', args.reload);
+% %             save(savefilename,'d')
+% %             disp(['ghetto spike data for ch ' num2str(channel) ' saved!'])
+% %         end
+% %         
+% %         if args.lfp 
+% %             d = d.load('lfp',args.lfp,'channels',channel,'reload', args.reload);
+% %             save(savefilename,'d')
+% %             disp(['lfp data for ch ' num2str(channel) ' saved!'])
+% %         end
+% %     end
+% end
 
 
 % step 5: run the analysis file for the day
