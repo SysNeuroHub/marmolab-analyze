5f2c72c47b84b568ef0457ac80d636d784b6bf5a
Maureen Hagan <maureen.hagan@monash.edu>
2023-02-14 16:03:19 +1100

    added branch instructions for marmodata

--- makeMdm.m
+++ makeMdm.m
@@ -8,10 +8,18 @@ function d = makeMdm(varargin)
 % if so, dont need to reload channels. save after every channel so dont
 % have to start over if it crashes. option to run analysis file
 
-% TODO: 
-% handle multiple files of the same paradigm
-% check for exisiting MDM file, load it and pick up where left off
-% deal with reload 
+% NOTE: this requries the "new-args" branch of marmodata. Other branches of marmodata dont handle loading spikes and lfps simultaneously.
+% the main changes in this branch are 
+% 1. the 'source' argument is gone. ie instead of
+%   loadArgs = {...,'spikes','true','source','ghetto',...}
+% you now will jsut call
+%    loadArgs = {...,'spikes','ghetto','lfp','raw',...}
+% 2. a cfg file is required for loading neural data. because we're behind the rest
+% of the lab, this will have to be supplied in loadArgs:
+%   'marmodata.cfgs.mbi.A1x32_100um' for centre array 
+%   'marmodata.cfgs.mbi.A1x32_Edge_100um' for edge array
+% 3. 'loadEye' is now just 'eye'
+
 
 % 2023-01-09 - Maureen Hagan <maureen.hagan@monash.edu>
 
@@ -162,6 +170,8 @@ end
 
 if ~isempty(args.analysis)
     d = feval(args.analysis, d);
-    save(savefilename,'d')
-    disp('analysis file run and saved!')
+    fprintf('analysis file run!')
 end
+
+save(savefilename,'d')
+fprintf('everything saved!')
