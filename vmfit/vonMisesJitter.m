25de6f689f76e84c8f05b8d7c57f925179614c56
Joanita <joanita.dsouza1@monash.edu>
2023-07-12 12:17:53 +1000

    Jos changes to make it work for her code?

--- vmfit/vonMisesJitter.m
+++ vmfit/vonMisesJitter.m
@@ -9,8 +9,6 @@ p = inputParser();
 % p.addParameter('jittersize',[]);
 p.addParameter('baselinecomparison',false,@(x) validateattributes(x,{'logical'},{'nonempty'}));
 p.addParameter('gamma',false,@(x) validateattributes(x,{'logical'},{'nonempty'}));
-p.addParameter('phatalt_start',[0 0 0 0],@(x) validateattributes(x,{'numeric'}));
-p.addParameter('phatnull_start',[0],@(x) validateattributes(x,{'numeric'}));
 
 p.parse(varargin{:});
 
@@ -30,7 +28,7 @@ while check && ind < 1e2
     if ~isnan(Datatmp.pci_alt), check = 0; end
 end
 if ~isnan(Datatmp.pci_alt)
-    Data = vonMisesFit(X,Ang,'phatalt_start',Datatmp.phat_alt,'baselinecomparison',args.baselinecomparison,'gamma',args.gamma);
+    Data = vonMisesFitGamma(X,Ang,Datatmp.phat_null,Datatmp.phat_alt);
 else
     disp('jitter didnt work!')
     Data = Datatmp;
