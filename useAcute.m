652d11ccaa526934dbd4095e716780ed9cfec756
Elizabeth Zavitz <elizabeth.zavitz@monash.edu>
2023-08-17 09:46:21 +1000

    now with more user friendly

--- useAcute.m
+++ useAcute.m
@@ -7,7 +7,18 @@ addpath(genpath('~/Documents/code/ephys/marmolab-analyze/'))
 dataloc = '~/Documents/code/ephys/corrStructure_V1MT/data/CJ223/001/CJ223.motionStim.055339.mat';
 saveloc = '~/Documents/code/ephys/corrStructure_V1MT/data/CJ223/001/';
 
+suffix = '_MT';
+
+switch suffix
+    case '_MT'
+        ch = 65:128;
+    case '_V1'
+        ch = 1:64;
+    otherwise 
+        ch = 1;
+end        
+        
 d= makeMdm_acute('path', dataloc, 'savepath', saveloc, ...
-               'channels', 65:128, 'spikes', 'ghetto', 'lfp', ...
+               'channels', ch, 'spikes', 'ghetto', 'lfp', ...
                'raw', 'cfg', 'marmodata.cfgs.acute.H64FlexiH64FlexiIntan',...
-               'savesuffix', '_MT');
+               'savesuffix', suffix);
