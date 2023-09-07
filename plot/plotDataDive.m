c7822b1a40d434352a84e21fc10bb740340264e4
Elizabeth Zavitz <elizabeth.zavitz@monash.edu>
2023-09-07 15:03:41 +1000

    Using Mau's method instead of GP + trying to adjust channel order

--- plot/plotDataDive.m
+++ plot/plotDataDive.m
@@ -70,57 +70,60 @@ paradigm = o.paradigm;
 % print([args.savefilepath date_str '_cuedvsuncuedpsths.png'],'-dpng')
 % end
 
-%% step 4 - get spike times and calc spike-phase relationships - generalized phase method
-
-
-% spikephase = trialSpikeLFPPhase(o,'onset','sTarget','trind', o.complete, 'bn',[-500,500],'method','GP','fk',[5, 40]);
-spikephase = trialSpikeLFPPhase(o,'bn',[0,1366],'method','GP','fk',[5,40]);
-spikephase = squeeze(spikephase);
-% plot it...
-
-SPI = nan(numel(channelOrder),numel(channelOrder)); % spike phase index (circ_r)
-PMP = nan(numel(channelOrder),numel(channelOrder)); % preferred mean phase (circ_m)
-
-% now we want to do this ordered by depth
-for icell = 1:numel(channelOrder)
-    cellind = channelOrder(icell);
-    for ilfp = 1:numel(channelOrder)
-        lfpind = channelOrder(ilfp); 
-        if ~isempty([spikephase{cellind,lfpind}{:}])
-        SPI(icell,ilfp) = circ_r([spikephase{cellind,lfpind}{:}]');
-        PMP(icell,ilfp) = circ_mean([spikephase{cellind,lfpind}{:}]');
-        disp(['icell = ' num2str(icell) ' lfp = ' num2str(ilfp) ' cicmean = ' num2str(PMP(icell,ilfp))])
-        end
-    end
-end
-
-figure; 
-subplot(1,2,1)
-imagesc(SPI,[0, 0.1]), colorbar
-map = colorcet( 'L03' ); colormap(gca,map)
-axis square
-title('Spike Phase Index')
-ylabel('Multiunits by depth')
-xlabel('LFPs by depth')
-
-subplot(1,2,2) 
-imagesc(PMP), colorbar
-map = colorcet( 'C2' ); colormap(gca, circshift( map, [ 28, 0 ] ) )
-axis square
-title('Phase Mean Angle')
-ylabel('Multiunits by depth')
-xlabel('LFPs by depth')
-
-if ~isempty(args.savefilepath)
-print([args.savefilepath date_str '_' subject '_' paradigm '_ZacSpikePhasebyDepth.png'],'-dpng')
-end
+% %% step 4 - get spike times and calc spike-phase relationships - generalized phase method
+% 
+% % spikephase = trialSpikeLFPPhase(o,'onset','sTarget','trind', o.complete, 'bn',[-500,500],'method','GP','fk',[5, 40]);
+% spikephase = trialSpikeLFPPhase(o,'bn',[0,1000],'method','GP','fk',[5,40]);
+% spikephase = squeeze(spikephase);
+% % plot it...
+% 
+% SPI = nan(numel(channelOrder),numel(channelOrder)); % spike phase index (circ_r)
+% PMP = nan(numel(channelOrder),numel(channelOrder)); % preferred mean phase (circ_m)
+% 
+% % now we want to do this ordered by depth
+% for icell = 1:numel(channelOrder)
+%     cellind = channelOrder(icell);
+%     for ilfp = 1:numel(channelOrder)
+%         lfpind = channelOrder(ilfp); 
+%         if ~isempty([spikephase{cellind,lfpind}{:}])
+%         SPI(icell,ilfp) = circ_r([spikephase{cellind,lfpind}{:}]');
+%         PMP(icell,ilfp) = circ_mean([spikephase{cellind,lfpind}{:}]');
+%         disp(['icell = ' num2str(icell) ' lfp = ' num2str(ilfp) ' cicmean = ' num2str(PMP(icell,ilfp))])
+%         end
+%     end
+% end
+% 
+% figure; 
+% subplot(1,2,1)
+% imagesc(SPI,[0, 0.1]), colorbar
+% map = colorcet( 'L03' ); colormap(gca,map)
+% axis square
+% title('Spike Phase Index')
+% ylabel('Multiunits by depth')
+% xlabel('LFPs by depth')
+% 
+% subplot(1,2,2) 
+% imagesc(PMP), colorbar
+% map = colorcet( 'C2' ); colormap(gca, circshift( map, [ 28, 0 ] ) )
+% axis square
+% title('Phase Mean Angle')
+% ylabel('Multiunits by depth')
+% xlabel('LFPs by depth')
+% 
+% if ~isempty(args.savefilepath)
+% print([args.savefilepath date_str '_' subject '_' paradigm '_ZacSpikePhasebyDepth.png'],'-dpng')
+% end
 
 
 %% use multitaper - test different frequency bands
 
 % spikephase = trialSpikeLFPPhase(o,'onset','sTarget', 'bn',[-500,500],'method','MT','tapers',[0.5,10],'fk',[4 12 20 40]);
-spikephase = trialSpikeLFPPhase(o,'bn',[0,500],'method','GP','tapers',[0.5,10],'fk',[4 12 20 40]);
+% spikephase = trialSpikeLFPPhase(o,'bn',[0,1000],'method','MT','tapers',[0.5,10],'fk',[4 12 20 40]);
+spikephase = trialSpikeLFPPhase(o,'bn',[0,1000],'method','MT','tapers',[0.5,10],'fk',4);
 spikephase = squeeze(spikephase);
+
+array = marmodata.cfgs.acute.H64FlexiH64FlexiIntan();
+channelOrder = array.electrode{1,1}.chanMap;
 
 nfreq = 4;
 SPI = nan(nfreq, numel(channelOrder),numel(channelOrder)); % spike phase index (circ_r)
